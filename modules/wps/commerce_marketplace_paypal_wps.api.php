40debf3bc65ae55b2ac679c980b5ee1687aeda8f
Maciej Zgadzaj <maciej.zgadzaj@gmail.com>
2015-04-17 13:22:36 -0400

    Refactoring

---
+++ modules/wps/commerce_marketplace_paypal_wps.api.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * Documents hooks provided by the Commerce Marketplace PayPal module.
+ */
+
+function hook_commerce_marketplace_paypal_wps_adaptive_get_paykey_alter(&$pay_request, $order, $payment_method) {
+  // No example.
+}
